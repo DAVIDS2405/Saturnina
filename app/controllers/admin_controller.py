8214b3a733746c254f4a54a1e5bce99437821d00
David Basantes <sebastian2405lucero@hotmail.com>
2024-01-12 07:12:31 -0500

    Fix Update products

--- app/controllers/admin_controller.py
+++ app/controllers/admin_controller.py
@@ -151,7 +151,7 @@ async def Create_products(data,imagen_producto):
  
 
     data_product = {"name": nombre_producto, "category": id_categoria,
-                    "descripcion": data.descripcion, "precio": data.precio, "imagen": cloudinary_data, "tallas": data.tallas, "colores":data.colores}
+                    "descripcion": data.descripcion, "precio": data.precio, "imagen": cloudinary_data, "tallas": data.tallas.value, "colores":data.colores}
     
 
     await User_Db.create("product",data_product)
@@ -207,11 +207,11 @@ async def Update_products(id_product,data,imagen_producto):
             cloudinary_data.append(data_cloudinary_filtered)
         
 
-        await User_Db.query('update ($id) merge {"name":($new_name_product),"precio":($new_price),"descripcion":($new_descripcion),"category":($new_category),"imagen":($new_image),"colores":($colores_new),"tallas":(new_tallas)};' ,{"id":id_product, "new_name_product":data.nombre_producto,"new_price":data.precio,"new_descripcion":data.descripcion,"new_category":data.id_categoria,"new_image":cloudinary_data,"new_tallas":data.tallas,"colores_new":data.colores})
+        await User_Db.query('update ($id) merge {"name":($new_name_product),"precio":($new_price),"descripcion":($new_descripcion),"category":($new_category),"imagen":($new_image),"colores":($colores_new),"tallas":(new_tallas)};' ,{"id":id_product, "new_name_product":data.nombre_producto,"new_price":data.precio,"new_descripcion":data.descripcion,"new_category":data.id_categoria,"new_image":cloudinary_data,"new_tallas":data.tallas.value,"colores_new":data.colores})
         await User_Db.close()
         raise HTTPException(status_code=status.HTTP_202_ACCEPTED,detail={"msg":"Tu product.o se ha actualizado"})
     
-    await User_Db.query('update ($id) merge {"name":($new_name_product),"precio":($new_price),"descripcion":($new_descripcion),"category":($new_category),"colores":($colores_new),"tallas":(new_tallas)};', {"id": id_product, "new_name_product": data.nombre_producto, "new_price": data.precio, "new_descripcion": data.descripcion, "new_category": data.id_categoria,"new_tallas":data.tallas,"colores_new":data.colores})
+    await User_Db.query('update ($id) merge {"name":($new_name_product),"precio":($new_price),"descripcion":($new_descripcion),"category":($new_category),"colores":($colores_new),"tallas":(new_tallas)};', {"id": id_product, "new_name_product": data.nombre_producto, "new_price": data.precio, "new_descripcion": data.descripcion, "new_category": data.id_categoria,"new_tallas":data.tallas.value,"colores_new":data.colores})
     await User_Db.close()
     raise HTTPException(status_code=status.HTTP_202_ACCEPTED,detail={"msg":"Tu producto se ha actualizado"})
     

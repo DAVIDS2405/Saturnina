73ef5df36c33253bab2706010c4941b721449763
David Basantes <sebastian2405lucero@hotmail.com>
2024-03-26 16:29:04 -0500

    Change server file

--- app/server.py
+++ app/server.py
@@ -1,9 +1,7 @@
-from fastapi import FastAPI, Request, status
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse, RedirectResponse
-from routers import user_routers,admin_routers
+from fastapi.responses import RedirectResponse
+from routers import user_routers, admin_routers
 
 app = FastAPI(
     docs_url="/api/v1/docs",
@@ -12,20 +10,20 @@ app = FastAPI(
     version="1.0",
     openapi_url="/api/v1/openapi.json",
     redoc_url=None,
-    
-)   
+
+)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-app.include_router(user_routers.router,prefix="/api/v1")
-app.include_router(admin_routers.router,prefix="/api/v1")
+app.include_router(user_routers.router, prefix="/api/v1")
+app.include_router(admin_routers.router, prefix="/api/v1")
 
 
-@app.get("/",include_in_schema=False)
+@app.get("/", include_in_schema=False)
 async def read_root():
     return RedirectResponse("/api/v1/docs")
